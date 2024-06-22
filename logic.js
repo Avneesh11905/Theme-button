becba10ff2b46a9a6de34da66f13d25adf787624
Avneesh11905 <avneeshprojects4257@gmail.com>
2024-06-23 00:20:13 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,27 @@
+let sw=0
+var r = document.querySelector(':root');
+document.getElementById("but").className="buttonTheme"
+
+
+function light(sw) {
+  
+  if (sw%2 == 0) {
+    document.getElementById("para").className="slideDay"
+    r.style.setProperty('--color1','skyblue');
+    r.style.setProperty('--color2','#357bb3');
+    r.style.setProperty('--color3','rgb(12, 0, 41)');
+    document.getElementById("text").innerHTML="Light Mode"
+    r.style.setProperty('--color4','black');
+    
+    
+  } else if(sw%2 == 1) {
+    document.getElementById("para").className="slideNight"
+    r.style.setProperty('--color1','rgb(41, 44, 62)');
+    r.style.setProperty('--color2','rgb(20, 19, 39)');
+    r.style.setProperty('--color3','rgb(0, 0, 0)');
+    r.style.setProperty('--color4','white');
+    document.getElementById("text").innerHTML="Dark Mode"
+    
+  }
+}
+
